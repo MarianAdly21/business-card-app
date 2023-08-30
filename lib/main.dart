101fb243bb16139b8899cdd4239e6ac72cd6215c
monamazen <marianadly2004@gmail.com>
2023-08-30 16:57:05 +0300

    used ListTile widget instead Row widget

--- lib/main.dart
+++ lib/main.dart
@@ -9,14 +9,14 @@ class BusinessApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        backgroundColor: const Color(0xFF2B475E),
+        backgroundColor: Color(0xFF2B475E),
         body: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const CircleAvatar(
+            CircleAvatar(
               radius: 122,
               backgroundColor: Colors.white,
               child: CircleAvatar(
@@ -25,77 +25,48 @@ class BusinessApp extends StatelessWidget {
                     'images/WhatsApp Image 2023-07-25 at 20.17.56.jpg'),
               ),
             ),
-            const Text(
+            Text(
               'Marian Adly',
               style: TextStyle(color: Colors.white, fontSize: 30),
             ),
-            const Text(
+            Text(
               'FLUTTER DEVELOPER',
               style: TextStyle(
                   color: Color(0xFF6C8090),
                   fontSize: 18,
                   fontWeight: FontWeight.bold),
             ),
-            const Divider(
+            Divider(
               color: Color(0xFF6C8090),
               thickness: 1,
               indent: 50,
               endIndent: 50,
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: ListTile(
+                leading: Icon(
+                  Icons.phone,
+                  color: Color(0xFF2B475E),
+                  size: 33,
                 ),
-                height: 65,
-                child: const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 16),
-                      child: Icon(
-                        Icons.phone,
-                        color: Color(0xFF2B475E),
-                        size: 33,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 16),
-                      child: Text('0114 8393 222',
-                          style: TextStyle(color: Colors.black, fontSize: 24)),
-                    ),
-                  ],
+                title: Text(
+                  '01148393222',
+                  style: TextStyle(color: Colors.black, fontSize: 24),
                 ),
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 5),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+              child: ListTile(
+                leading: Icon(
+                  Icons.email,
+                  color: Color(0xFF2B475E),
+                  size: 33,
                 ),
-                height: 65,
-                child: const Row(
-                  children: [
-                    Padding(
-                      padding: EdgeInsets.only(left: 16),
-                      child: Icon(
-                        Icons.mail,
-                        color: Color(0xFF2B475E),
-                        size: 33,
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.only(left: 16),
-                      child: Text('Marianadly@gmail.com',
-                          style: TextStyle(color: Colors.black, fontSize: 24)),
-                    ),
-                    Spacer(
-                      flex: 2,
-                    ),
-                  ],
+                title: Text(
+                  'Marianadly@gmail.com',
+                  style: TextStyle(color: Colors.black, fontSize: 24),
                 ),
               ),
             ),
